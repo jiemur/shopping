f84c8249eafce0b4e86bc06972d742d31b79e462
jiemur <1306068995@qq.com>
2019-02-24 15:09:26 +0800

    first

---
+++ src/main/java/com/wzn/ShoppingApplication.java
@@ -0,0 +1,16 @@
+package com.wzn;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApplication.class, args);
+    }
+
+}
